251b125140748e51bda493b081d00a6228853fc5
DAHLS <dahl.asbjorn@gmail.com>
2025-08-28 19:06:08 +0200

    more householding

--- AOE_BoW.py
+++ AOE_BoW.py
@@ -8,6 +8,7 @@ Created on Mon Aug 25 15:49:15 2025
 
 import pandas as pd
 import joblib
+import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score, classification_report
@@ -28,8 +29,9 @@ knn.fit(X_train, y_train)
 y_pred = knn.predict(X_test)
 
 #Save trained model and tfidf vectorization
-#joblib.dump(vectorizer, 'orgaffil_tfidf-bow.pkl')
-#joblib.dump(knn, 'orgafill_kNN-model.pkl')
+timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+joblib.dump(vectorizer, f'models/AOE_tfidf-bow_{timestamp}.pkl')
+joblib.dump(knn, f'models/AOE_kNN-model_{timestamp}.pkl')
 
 print(f"Accuracy: {accuracy_score(y_test, y_pred):.2f}")
 print("\nClassification Report:")
--- AOE_predictor.py
+++ AOE_predictor.py
@@ -8,14 +8,14 @@ Created on Mon Aug 25 17:16:12 2025
 
 import pandas as pd
 import joblib
-from sklearn.feature_extraction.text import TfidfVectorizer
 
 new_df = pd.read_excel('Slutsoegning/2025/slutsogning_2025_spring_wip.xlsx')
 new_df['Text'] = new_df[['AU', 'TI', 'JN']].apply(
     lambda row: ' '.join(row.dropna()), axis=1)
 
-knn = joblib.load('orgafill_kNN-model.pkl')
-vectorizer = joblib.load('orgaffil_tfidf-bow.pkl')
+#TODO add more flexiable model picking
+knn = joblib.load('models/orgafill_kNN-model.pkl')
+vectorizer = joblib.load('models/orgaffil_tfidf-bow.pkl')
 X_new = vectorizer.transform(new_df['Text'])
 
 predicted_labels = knn.predict(X_new)
